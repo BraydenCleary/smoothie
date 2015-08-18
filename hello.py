f8bf0d3742bfb84403b7f3387ba2a86126cbcc69
Brayden Cleary <brayden.cleary@gmail.com>
2015-08-17 17:35:17 -0700

    fdsa

--- hello.py
+++ hello.py
@@ -1,9 +1,10 @@
 import os
 from flask import Flask
 from flask import render_template
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def map():
-  return render_template('map.html', name='Brayden')
+  return render_template('map.html', now=datetime.now())
