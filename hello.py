f4012343e481bf2f93ff26c3e49523e310121c7c
Brayden Cleary <brayden.cleary@gmail.com>
2015-08-17 17:04:25 -0700

    fixing

--- hello.py
+++ hello.py
@@ -1,9 +1,10 @@
 import os
 from flask import Flask
-from datetime import datetime0
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def map():
-  "The time is " + str(datetime.now())
+  # "The time is " + str(datetime.now())
+  render_template('map.html', name='Brayden')
